73939aad114c4da15400fda6f6f0ab2f2fb7f704
Roberto Garrido Trillo <robegt72@gmail.com>
2023-02-04 06:41:40 +0100

    initial commit

--- projects/clima/src/app/services/clima.service.ts
+++ projects/clima/src/app/services/clima.service.ts
@@ -10,7 +10,7 @@ export class ClimaService {
   /* https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} */
 
   url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-  key = '4ac2b3a45d4d45c02a43355e4c73abb9';
+  key = '';
 
 
   constructor(private http: HttpClient) {
